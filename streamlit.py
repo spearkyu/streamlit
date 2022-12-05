3489b32dcf97e474d4e3e79ed64ccdddc38c14b2
Kyuchang Kim <104061551+spearkyu@users.noreply.github.com>
2022-12-05 16:41:09 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -29,7 +29,7 @@ body = json_ob['response']['body']['items']
 # # Dataframe으로 만들기
 dataframe = pd.DataFrame(body)
 # # key 값 int으로 만들기
-# dataframe['total'] = pd.to_numeric(dataframe['khaiValue'])
+dataframe['total'] = pd.to_numeric(dataframe['khaiValue'])
 dataframe['dust'] = pd.to_numeric(dataframe['pm10Value'])
 time = dataframe['dataTime']
 total = dataframe['total']
@@ -38,3 +38,5 @@ dust = dataframe['dust']
 st.write(total)
 st.bar_chart(total)
 st.bar_chart(dust)
+print(total)
+print(dust)
