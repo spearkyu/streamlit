7310c50c6f148ac04cbcc11e3e3f0e7385148f9d
Kyuchang Kim <104061551+spearkyu@users.noreply.github.com>
2022-12-05 19:25:04 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,9 @@ import streamlit as st
 import altair as alt
 file_path = "C:\\Users\hojin\Desktop\gwajea\python\gimal\simple.txt"
 url = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName=백마로(마두역)&dataTerm=DAILY&pageNo=1&numOfRows=100&returnType=json&serviceKey=E6YZt5LknsrARD2OScTh1asLoxZEksRJ%2BmmqsJr0hvdlXM0RAex8fUJfQqFPNJ9JgUh4UmnP049AaTqXjFuf9w%3D%3D"
+image1 = "https://github.com/spearkyu/streamlit/blob/main/dust_v.jpg?raw=true"
+image2 = "https://github.com/spearkyu/streamlit/blob/main/o3.png?raw=true"
+image3 = "https://github.com/spearkyu/streamlit/blob/main/total.jpg?raw=true"
 
 response = requests.get(url)
 
@@ -30,17 +33,29 @@ body = json_ob['response']['body']['items']
 dataframe = pd.DataFrame(body)
 # # key 값 int으로 만들기
 dataframe['total'] = pd.to_numeric(dataframe['khaiValue'])
-dataframe['dust'] = pd.to_numeric(dataframe['pm10Value'])
-# name = dataframe['stationName']
+dataframe['dust_v'] = pd.to_numeric(dataframe['pm10Value'])
+dataframe['dust_g'] = pd.to_numeric(dataframe['pm10Grade'])
+dataframe['co'] = pd.to_numeric(dataframe['coValue'])
+dataframe['o3'] = pd.to_numeric(dataframe['o3Value'])
 total = dataframe['total']
-dust = dataframe['dust']
+dust_v = dataframe['dust_v']
+dust_g = dataframe['dust_g']
+co = dataframe['co']
+o3 = dataframe['o3']
 # # 바차트 올리기
-st.title('공공데이터 분석하기')
-st.write('한국환경공단_에어코리아_대기오염정보의 통합환경수치와 미세먼지를 불러와 분석을 진행합니다.')
-st.write('파주시의 대기 상태를 실시간으로 불러와 시각화 하고있습니다.')
-st.header("통합 환경 수치")
+st.title('공공데이터 시각화')
+st.write('공공데이터에 있는 에어코리아 대기오염정보에서 백마로(마두역)의 통합오염수치와 미세먼지농도/지수, 오존농도, 일산화탄소농도를 불러와 자료로 사용했습니다.')
+st.write('일산 백마로의 대기오염상태를 시각화하여 실시간으로 상태를 나타내 줍니다.')
+st.header("미세먼지농도")
+st.bar_chart(dust_v)
+st.image(image1)
+st.header("미세먼지지수")
+st.bar_chart(dust_g)
+st.header("오존농도")
+st.bar_chart(o3)
+st.image(image2)
+st.header("일산화탄소농도")
+st.bar_chart(co)
+st.header("통합오염지수")
 st.bar_chart(total)
-#st.image(image2)
-st.header("미세먼지")
-st.bar_chart(dust)
-#st.image(image)
+st.image(image3)
